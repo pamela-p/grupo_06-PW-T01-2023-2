d56a63ca6786b704763f7041501214e891ac36dd
Everton Bonfim Gomes <131558782+evertonbgomes@users.noreply.github.com>
2023-10-15 13:17:54 -0400

    mudanças de referências e atualização de scripts
    Troca das referências para o scripts e css, adição de tratativas nos scripts para visulizar senhas, quantidade minima de caracteres de senha e email, adição visual para a semelhança na verificação de senha.

--- This or That/scripts/script_login.js
+++ This or That/scripts/script_login.js
@@ -1,33 +1,83 @@
-function validarLogin(){
+let btn = document.querySelector('#verSenha')
+let senha = document.querySelector('#senha')
 
-    // variáveis de controle 
-    const form = document.querySelector('form');
-    const emailInput = document.getElementById('email');
-    const senhaInput = document.getElementById('senha');
+let labelSenha = document.querySelector('#senhaLabel')
+let validSenha = false
 
-    // ver se tem campo vazio
-    if(emailInput.value.trim() === ''){
-        alert('Preencha o campo E-mail.');
-        return;
-    }
-
-    if(senhaInput.value.trim() === ''){
-        alert('Preencha o campo Senha.');
-        return;
-    }
-
-    /*ver se email e senha coincidem com o que está no banco de dados*/
-    if(enviarLogin()){
-        window.location.href = 'index.html'; //redireciona para pagina do jogo
-    }
-
-}
-
-function enviarLogin(){
-    /*ver se email e senha coincidem com o que está no banco de dados*/
-    return true;
-}
+btn.addEventListener('click', ()=>{
+  let inputSenha = document.querySelector('#senha')
+  
+  if(inputSenha.getAttribute('type') == 'password'){
+    inputSenha.setAttribute('type', 'text')
+  } else {
+    inputSenha.setAttribute('type', 'password')
+  }
+})
 
 function criarUsuario(){
-    window.location.href = 'cadastro.html';
+  window.location.href = 'cadastro.html';
+}
+
+senha.addEventListener('keyup', () => {
+  if(senha.value.length <= 5){
+    labelSenha.setAttribute('style', 'color: red')
+    labelSenha.innerHTML = 'Senha *Insira no minimo 6 caracteres'
+    senha.setAttribute('style', 'border-color: red')
+    validSenha = false
+  } else {
+    labelSenha.setAttribute('style', 'color: green')
+    labelSenha.innerHTML = 'Senha'
+    senha.setAttribute('style', 'border-color: green')
+    validSenha = true
+  }
+})
+
+function entrar(){
+  let usuario = document.querySelector('#email')
+  let userLabel = document.querySelector('#emailLabel')
+  
+  let senha = document.querySelector('#senha')
+  let senhaLabel = document.querySelector('#senhaLabel')
+  
+  let msgError = document.querySelector('#msgError')
+  let listaUser = []
+  
+  let userValid = {
+    nome: '',
+    user: '',
+    senha: ''
+  }
+  
+  listaUser = JSON.parse(localStorage.getItem('listaUser'))
+  
+  listaUser.forEach((item) => {
+    if(usuario.value == item.userCad && senha.value == item.senhaCad){
+       
+      userValid = {
+         nome: item.nomeCad,
+         user: item.userCad,
+         senha: item.senhaCad
+       }
+      
+    }
+  })
+   
+  if(usuario.value == userValid.user && senha.value == userValid.senha){
+    window.location.href = 'index.html'
+    
+    let mathRandom = Math.random().toString(16).substr(2)
+    let token = mathRandom + mathRandom
+    
+    localStorage.setItem('token', token)
+    localStorage.setItem('userLogado', JSON.stringify(userValid))
+  } else {
+    userLabel.setAttribute('style', 'color: red')
+    usuario.setAttribute('style', 'border-color: red')
+    senhaLabel.setAttribute('style', 'color: red')
+    senha.setAttribute('style', 'border-color: red')
+    msgError.setAttribute('style', 'display: block')
+    msgError.innerHTML = 'Usuário ou senha incorretos'
+    usuario.focus()
+  }
+  
 }
